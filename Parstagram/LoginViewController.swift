99c26cd3d9ac75b2d8d63fb05b12cb08db28208e
Vicky Zheng <vickyjmzheng@gmail.com>
2020-10-23 13:03:40 -0400

    finished authentication

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -6,15 +6,46 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var usernameField: UITextField!
     
     @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground {
+            (succeeded: Bool, error: Error?) -> Void in
+            if let error = error {
+                let errorString = error.localizedDescription
+                print("Error: \(errorString)")
+                // Show the errorString somewhere and let the user try again.
+            } else {
+                // Hooray! Let them use the app now.
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    
     }
     
     @IBAction func onSignIn(_ sender: Any) {
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else {
+                let errorString = error!.localizedDescription
+                print("Error: \(errorString)")
+            }
+        }
+    
     }
     
     
