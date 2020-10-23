3764101bd677bc1e988a4af5b93cfca304a36900
Vicky Zheng <vickyjmzheng@gmail.com>
2020-10-23 12:05:37 -0400

    set up parse server

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "0lFich1X8cecFVAUbXIARrn4hG3AiRf51fn3jD09" // <- UPDATE
+                $0.clientKey = "td0LdFWinz165SXBRCtQggLSPe7oJV0wKLpoI4ol" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
